b9e7e497a77b2f7cfc8fa9cd1a9e1f0b9dda8b88
13580491277 <465832455@qq.com>
2024-10-07 18:07:11 +0800

    update

--- home.py
+++ home.py
@@ -41,6 +41,37 @@ file = st.sidebar.file_uploader(".xlsx")
 char_options = ['多线图','散点图','气泡图','柱状图']
 char_type = st.sidebar.selectbox('请选择绘图类型',char_options,placeholder='选择一个类型')
 
+if char_type == '气泡图':
+    # chart_data = pd.DataFrame([[4,4,4],[3,3,3],[2,2,2],[4,4,4,],[3,3,3],[2,2,2]],columns=['张三','李四','王五'])
+    # st.scatter_chart(chart_data,x_label="日期",y_label="可用人天")
+    x = ['1号', '2号', '3号','4号','5号','6号','7号','8号','9号','10号','11号','12号','13号','14号','15号','16号','17号','18号','19号','20号','21号','22号','23号','24号','25号','26号','27号','28号','29号','30号','31号']
+    y1 = [5, 4, 3]
+    y2 = [4, 3, 2]
+    y3 = [5, 4.5, 4]
+    trace1 = go.Scatter(
+        x=x,
+        y=y1,
+        mode="markers+lines",
+        name='张三',
+        marker={"size":8,"symbol":"square"}
+    )
+    trace2 = go.Scatter(
+        x=x,
+        y=y2,
+        mode='markers+lines',
+        name='李四',
+        marker={"size":8,"symbol":"square"}
+    )
+    trace3 = go.Scatter(
+        x=x,
+        y=y3,
+        mode='markers+lines',
+        name='王五',
+        marker={"size":8,"symbol":"square"}
+    )
+    fig = go.Figure(data=[trace1, trace2, trace3],
+                    layout={"title": "本月可用资源燃尽图（数据为示意）", "xaxis_title": "日期", "yaxis_title": "可用人天","xaxis":{"tickangle":-60}})
+    st.plotly_chart(fig, theme='streamlit')
 
 if file:
     #st.write('你选择的文件是：',file)
@@ -75,7 +106,7 @@ if file:
             fig = px.line(sub_df, x=df['time'], y="交付总成本", width=1080, height=550)
             fig.update_layout(legend=dict(orientation="h", ))  # 开启水平显示
             st.plotly_chart(fig, theme='streamlit')
-
+            print(sub_df)
     if char_type == '散点图':
         if len(col_list) > 1:
             sub_df = df[col_list_bak]
@@ -85,9 +116,6 @@ if file:
             trace1 = go.Bar(x=column0_list, y=column1_list, name='需验收总额（元）')
             fig = go.Figure(data=[trace1], layout={"template": "plotly_dark"})
             st.plotly_chart(fig, theme='streamlit')
-    if char_type == '气泡图':
-        chart_data = pd.DataFrame([[4,4,4],[3,3,3],[2,2,2]],columns=['张三','李四','王五'])
-        st.scatter_chart(chart_data,x_label="日期",y_label="可用人天")
 
 
 else:
